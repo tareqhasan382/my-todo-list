2ec012fd1a0b2452e69aba8acee47c874578589e
tareqhasan382 <tareqhasan382@gmail.com>
2022-03-06 04:23:29 +0600

    my todo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+     <p>Tareq Hasan</p>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
